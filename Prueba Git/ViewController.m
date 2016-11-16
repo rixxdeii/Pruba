46d8869dd9e14597c4f391277fa34e71491739c9
RicardoGonet <ricardogonet@macbook-pro-de-ricardogonet.local>
2016-11-16 16:17:10 -0600

    add

--- Prueba Git/ViewController.m
+++ Prueba Git/ViewController.m
@@ -15,6 +15,7 @@
 @implementation ViewController
 
 - (void)viewDidLoad {
+     NSString * rtstr=@"Master";
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
