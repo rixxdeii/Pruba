47e09fb358d5b9b375077df6d51326756acd18a5
RicardoGonet <ricardogonet@macbook-pro-de-ricardogonet.local>
2016-11-16 16:17:47 -0600

    add

--- Prueba Git/ViewController.m
+++ Prueba Git/ViewController.m
@@ -15,6 +15,7 @@
 @implementation ViewController
 
 - (void)viewDidLoad {
+     NSString * rtstr=@"Rama1";
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
