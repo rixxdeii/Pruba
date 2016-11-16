5370e423ba8e177c402de9ecfbbe1b338f8ba6ce
RicardoGonet <ricardogonet@macbook-pro-de-ricardogonet.local>
2016-11-16 16:16:34 -0600

    add

--- Prueba Git/ViewController.m
+++ Prueba Git/ViewController.m
@@ -2,8 +2,7 @@
 //  ViewController.m
 //  Prueba Git
 //
-//  Created by RicardoGonet on 16/11/16.
-//  Copyright © 2016 Ric. All rights reserved.Hola mundo master
+//  Created by RicardoGonet on 16/11/16.rama 2
 //
 
 #import "ViewController.h"
@@ -16,6 +15,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    NSString * rtstr=@"Rama2";
     // Do any additional setup after loading the view, typically from a nib.
 }
 
